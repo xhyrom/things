9670bd4f1a3e88e540538b783374e261ddebf9e7
xHyroM <generalkubo@gmail.com>
2023-06-17 22:39:50 +0200

    feat(configs): add tailwindcss config

---
+++ packages/configs/tailwind.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from "tailwindcss";
+import colors from "@xhyrom/colour-palette/colors.json" assert { type: "json" };
+
+export default (
+  options: Partial<{
+    colors?: Record<string, string>;
+  }> &
+    Config
+): Config => ({
+  theme: {
+    extend: {
+      colors: {
+        ...colors,
+        ...(options.colors ?? {}),
+      },
+    },
+  },
+  ...options,
+});
